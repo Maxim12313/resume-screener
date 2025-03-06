973fb7e7c2e861fd6c6cb173524bbc6f952b5980
maxim12313 <maximk@umich.edu>
2025-03-05 23:56:40 -0800

    improvements to code structure plus added details to chat for retrieved documents. Considering loading only previous exchange and keeping list of 2k resumes to track. for now also using dummy data to improve speed of tesitng and reduce api costs

--- project/database.py
+++ project/database.py
@@ -10,10 +10,10 @@ from config import RESUME_DOC_TABLE, EMBEDDING_TABLE, EMBEDDING_LENGTH, Shared
 
 shared = Shared()
 
+
 def embed_chunks_map(sample):
-    return {
-        "Embeddings" : shared.embed_documents(sample["Chunks"])
-    }
+    return {"Embeddings": shared.embed_documents(sample["Chunks"])}
+
 
 def process_data():
     df = pd.read_csv("UpdatedResumeDataSet.csv", encoding="utf-8")
@@ -30,7 +30,7 @@ def process_data():
 
 def embedding_db(df):
     with psycopg.connect("user=postgres") as conn:
-        conn.execute('CREATE EXTENSION IF NOT EXISTS vector')
+        conn.execute("CREATE EXTENSION IF NOT EXISTS vector")
         register_vector(conn)
         with conn.cursor() as cur:
             cur.execute(f"DROP TABLE IF EXISTS {EMBEDDING_TABLE}")
@@ -50,13 +50,14 @@ def embedding_db(df):
                 chunks = row["Chunks"]
                 for j in range(len(embeds)):
                     cur.execute(
-                        f"INSERT INTO {EMBEDDING_TABLE} (source_id, chunk, embedding) VALUES (%s, %s, %s)", 
-                        (i + 1, chunks[j], embeds[j]) 
+                        f"INSERT INTO {EMBEDDING_TABLE} (source_id, chunk, embedding) VALUES (%s, %s, %s)",
+                        (i + 1, chunks[j], embeds[j]),
                     )
                     # +1 because sql 1 indexed
 
             for x in cur.execute(f"SELECT id, source_id FROM {EMBEDDING_TABLE}"):
                 print(x)
+
 
 def doc_db(df):
     # going to ignore the embeddings
@@ -73,10 +74,44 @@ def doc_db(df):
             )
 
             for i, row in df.iterrows():
-                cur.execute(f"INSERT INTO {RESUME_DOC_TABLE} (resume) VALUES (%s)", (row["Clean"],))
+                cur.execute(
+                    f"INSERT INTO {RESUME_DOC_TABLE} (resume) VALUES (%s)",
+                    (row["Clean"],),
+                )
 
             for x in cur.execute(f"SELECT * FROM {RESUME_DOC_TABLE}"):
-                print(x)            
+                print(x)
+
+
+def get_top_k(embedding, k=10):
+    with psycopg.connect("user=postgres") as conn:
+        with conn.cursor() as cur:
+            res = cur.execute(
+                f"""SELECT *, embedding <-> %s::vector AS similarity
+                FROM {EMBEDDING_TABLE} ORDER BY similarity LIMIT %s""",
+                (embedding, k * 2),
+            ).fetchall()
+            # anticipate duplicates, maintain k ret, low prob fail
+
+            ids = set()
+            ans = []
+            for record in res:
+                if record[1] in ids:
+                    continue
+                if len(ans) == k:
+                    break
+                ans.append({"score": record[4], "source_id": record[1]})
+                ids.add(record[1])
+
+            for record in ans:
+                id = record["source_id"]
+                res = cur.execute(
+                    f"SELECT resume FROM {RESUME_DOC_TABLE} WHERE id = %s", (id,)
+                ).fetchone()[0]
+                record["resume"] = res
+
+            return ans
+
 
 def add_resume(text):
     cleaned = clean(text)
@@ -84,28 +119,34 @@ def add_resume(text):
     embeddings = shared.embed_documents(chunks)
     with psycopg.connect("user=postgres") as conn:
         with conn.cursor() as cur:
-            length = cur.execute(f"SELECT COUNT (*) FROM  {RESUME_DOC_TABLE}").fetchone()[0]
-            cur.execute(f"INSERT INTO {RESUME_DOC_TABLE} (resume) VALUES (%s)", (cleaned,))
+            length = cur.execute(
+                f"SELECT COUNT (*) FROM  {RESUME_DOC_TABLE}"
+            ).fetchone()[0]
+            cur.execute(
+                f"INSERT INTO {RESUME_DOC_TABLE} (resume) VALUES (%s)", (cleaned,)
+            )
 
             for i in range(len(chunks)):
                 cur.execute(
-                    f"INSERT INTO {EMBEDDING_TABLE} (source_id, chunk, embedding) VALUES (%s, %s, %s)", 
-                    (length, chunks[i], embeddings[i])
+                    f"INSERT INTO {EMBEDDING_TABLE} (source_id, chunk, embedding) VALUES (%s, %s, %s)",
+                    (length, chunks[i], embeddings[i]),
                 )
+
 
 def retrieve_resumes(ids: List[int]):
     with psycopg.connect("user=postgres") as conn:
         with conn.cursor() as cur:
             data = []
             for id in ids:
-                res = cur.execute(f"SELECT resume FROM {RESUME_DOC_TABLE} WHERE id = %s", (id,)).fetchone()[0]
+                res = cur.execute(
+                    f"SELECT resume FROM {RESUME_DOC_TABLE} WHERE id = %s", (id,)
+                ).fetchone()[0]
                 data.append(res)
-            
+
             return data
+
 
 if __name__ == "__main__":
     df = process_data()
     embedding_db(df)
     doc_db(df)
-
-    
--- project/queryMaker.py
+++ project/queryMaker.py
@@ -1,8 +1,6 @@
-import psycopg
-from typing import List
-from config import Shared, EMBEDDING_TABLE
-from database import retrieve_resumes
-
+from typing import List, Dict
+from config import Shared
+from database import get_top_k
 from dotenv import load_dotenv
 from openai import OpenAI
 
@@ -10,53 +8,52 @@ load_dotenv()
 shared = Shared()
 
 
-def load_resumes(ids: List[int], resumes: List[str]):
+def load_resumes(top_k: List[Dict]):
     data = ""
-    for i in range(len(ids)):
-        add = f"### Applicant ID {ids[i]}\n"
-        add += resumes[i]
+    for record in top_k:
+        add = f"### Applicant ID {record['source_id']}\n"
+        add += record["resume"]
         data += add
     return data
 
 
-def get_top_k(prompt: str, k=10):
-    prompt = shared.embed_query(prompt)
-    with psycopg.connect("user=postgres") as conn:
-        with conn.cursor() as cur:
-            res = cur.execute(
-                f"""SELECT *, embedding <-> %s::vector AS similarity FROM 
-                                {EMBEDDING_TABLE} ORDER BY similarity LIMIT %s""",
-                (prompt, k),
-            ).fetchall()
-            return [{"score": record[4], "source_id": record[1]} for record in res]
-
-
 class QueryMaker:
+    system_prompt = None
+    model = None
+    temperature = None
+    k = None
     client = OpenAI()
 
-    def query(
-        self, prompt: str, developer_prompt="", k=10, temperature=0, model="gpt-4o-mini"
-    ):
-        if not len(developer_prompt):
-            developer_prompt = "You are an expert in talent acquisition and tasked with analyzing and comparing a set resumes to select the best applicants for hire"
+    def __init__(self, k=10, temperature=0, model="gpt-4o-mini", system_prompt=""):
+        self.k = k
+        self.temperature = temperature
+        self.model = model
+        self.system_prompt = (
+            system_prompt
+            if len(system_prompt)
+            else (
+                "You are an expert in talent acquisition and tasked with analyzing and comparing"
+                "a set resumes to select the best applicants for hire"
+            )
+        )
 
-        top_k = get_top_k(prompt, k)
-        ids = list(set([vals["source_id"] for vals in top_k]))  # just get unique
-        resumes = retrieve_resumes(ids)
-        context = load_resumes(ids, resumes)
+    def query(self, prompt: str):
+        embedded = shared.embed_query(prompt)
+        top_k = get_top_k(embedded, self.k)
+        context = load_resumes(top_k)
 
         query_prompt = f"""## Context:\n{context}\n\n## Query: {prompt}"""
 
         completion = self.client.chat.completions.create(
-            model=model,
+            model=self.model,
             messages=[
                 {"role": "user", "content": query_prompt},  # task or main prompt
-                {"role": "system", "content": developer_prompt},  # personality
+                {"role": "system", "content": self.system_prompt},  # personality
             ],
-            temperature=temperature,  # how creative where 0 is deterministic
+            temperature=self.temperature,  # how creative where 0 is deterministic
             stream=True,
         )
-        return {"text": completion, "ids": ids, "resumes": resumes}
+        return {"text": completion, "top_k": top_k}
 
 
 if __name__ == "__main__":
